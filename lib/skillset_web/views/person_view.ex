b7c7da03c393327ed6d7d5dfeca3fb096d96afcf
majhogee <jo.wojtowicz@gmail.com>
2019-01-25 17:16:18 +0100

    Refactor listing skills for person

--- lib/skillset_web/views/person_view.ex
+++ lib/skillset_web/views/person_view.ex
@@ -1,3 +1,9 @@
 defmodule SkillsetWeb.PersonView do
   use SkillsetWeb, :view
+
+  def skills_for_person(person) do
+    person.insights
+    |> Enum.map(fn insight -> insight.skill.name end)
+    |> Enum.join(", ")
+  end
 end
